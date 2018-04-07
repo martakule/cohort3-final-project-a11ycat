155ef19de61954eb6fb01349aff75720c68385fe
Marta Kule <marta.kule@gmail.com>
2018-04-07 19:05:06 -0400

    Work in progress

--- client/src/features/app/app.component.js
+++ client/src/features/app/app.component.js
@@ -6,7 +6,6 @@ import { AutoCompleteListContainer } from '../auto-complete-list/auto-complete-l
 import { Link } from 'react-router-dom';
 import { Heading } from '../../ui-kit/heading';
 import { List } from '../../ui-kit/list';
-import { ToastContainer } from '../toast/toast.container';
 
 export class App extends Component {
   componentDidMount() {
@@ -30,10 +29,6 @@ export class App extends Component {
     const { showAutoComplete, listLocations } = this.props;
     return (
       <div className="App">
-        <ToastContainer
-          state="success"
-          message="Your review of Blah was successful."
-        />
         <SearchBarContainer />
         {showAutoComplete && <AutoCompleteListContainer />}
         <Heading> Nearby locations </Heading>
